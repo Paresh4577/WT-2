5817de08fda843ce0c80862eff64256a20ada0dd
Paresh4577 <142432711+Paresh4577@users.noreply.github.com>
2024-01-04 11:32:13 +0530

    Add files via upload

---
+++ ES6_Array_methods/1.js
@@ -0,0 +1,92 @@
+//This is Push Method
+// let a = [1,2,3,4,5,6];
+//   a.push(8);
+//  console.log(a);
+
+//This is Pop Method
+// let a = [1,2,3,4,5,6];
+//   a.pop();
+//  console.log(a);
+
+//This is Shift Method
+// let a = [1,2,3,4,5,6];
+//   a.shift();
+//  console.log(a);
+
+//This is Unshift Method
+// let a = [1,2,3,4,5,6];
+//   a.unshift(11);
+//  console.log(a);
+
+//This is Some Method
+// let a = [1,2,3,4,5,6];
+//  let even = (x)=> x%2==0;
+//  console.log(a.some(even));
+
+//This is Includes Method
+// let a = [1,2,3,4,5,6];
+// console.log(a.includes(11));//false
+
+//This is Every Method
+// let a = [1,2,3,4,5,6];
+// console.log(a.every((x)=>{
+//    return x>0;
+// }));
+
+//This is Filter Method
+// let a = [1,2,3,4,5,6];
+// console.log(a.filter((x)=>{
+//    return x>4;
+// }));
+
+//This is Map Method
+// let a = [1,2,3,4,5,6];
+// console.log(a.map((x)=>{
+//    return x*2;
+// }));
+
+//This is Find Method
+// let a = [1,2,3,4,5,6];
+// console.log(a.find((x)=>{
+//    return x>3;
+// }));
+
+//This is Slice Method
+// let a = [1,2,3,4,5,6];
+// let ans = a.slice(2,4);
+// console.log(ans);
+
+//This is Splice Method
+// let a = [1,2,3,4,5,6];
+// let ans = a.splice(2,4);
+// console.log(ans);
+
+//This is For Of Loop
+// let a = [11,2,32,45,5,6];
+// for(let ans of a){
+//     console.log(ans);
+// }
+
+//This is For in Loop
+// let a = [11,2,32,45,5,6];
+// for(let ans in a){
+//     console.log(ans);
+// }
+
+//This is Spread Operator
+// const a1 = ["Hello","I am"];
+// const a2 = ["Paresh","Mori"];
+// const a3 = ["From","Rajkot"];
+// const a4 = ["Thank","You"];
+
+// let ans = [...a1,...a2,...a3,...a4];
+// console.log(ans);
+
+
+//This is Reduce Method
+// let a = [1,2,3,4,5,6];
+// console.log(a.reduce((x,y)=>{
+//     return x+y;
+// }));
+
+
